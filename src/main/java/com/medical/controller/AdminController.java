202a73099693e2224464937ecaab104d9f8c46a6
27908 <2790802011@qq.com>
2022-08-15 09:33:36 +0800

    txx

--- src/main/java/com/medical/controller/AdminController.java
+++ src/main/java/com/medical/controller/AdminController.java
@@ -15,5 +15,5 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/medical/admin")
-public class AdminController {//撒大大x
+public class AdminController {//撒大大xc
 }
