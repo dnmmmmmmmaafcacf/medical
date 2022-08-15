f3cde770ef85372ac48895177c36540f774731cc
cxk-jinitaimei <111045964+cxk-jinitaimei@users.noreply.github.com>
2022-08-15 09:57:55 +0800

    test

--- src/main/java/com/medical/controller/UserController.java
+++ src/main/java/com/medical/controller/UserController.java
@@ -1,9 +1,22 @@
 package com.medical.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.medical.common.Result;
+import com.medical.entity.User;
+import com.medical.mapper.UserMapper;
+import com.medical.service.UserService;
+import com.medical.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.HtmlUtils;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.jws.soap.SOAPBinding;
+import javax.management.Query;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -16,5 +29,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medical/user")
 public class UserController {
-
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserMapper userMapper;
+    @PostMapping("/register")
+    @ResponseBody
+    public Result register(@RequestBody User user){
+        String name = user.getName();
+        String password = user.getPassword();
+        name = HtmlUtils.htmlEscape(name);  //可将HTML标签互相转义
+        user.setName(name);
+        user.setPassword(password);
+        boolean ex = userService.isExist(name);
+        if (ex){
+        Result result= new Result("0","用户名已被注册");
+        return result;
+        }else {
+            //调用业务层方法，插入到DB，统一处理异常
+            userService.save(user);
+            Result result = new Result("1", "注册成功");
+            return result;
+        }
+    }
+    @GetMapping("/select")
+        public List<User> select(User user){
+        QueryWrapper queryWrapper =new QueryWrapper();
+        queryWrapper.eq("int",user.getId());
+        List list = userMapper.selectList(queryWrapper);
+        return list;
+    }
+    /**
+     *
+     */
 }
